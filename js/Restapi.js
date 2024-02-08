7b658eb43bad970aa13405f32a4a4380ac982349
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-02-08 21:59:12 +0530

    checkin

---
+++ js/Restapi.js
@@ -0,0 +1,26 @@
+const localCountryApi = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  const countriesHTML = countries.map((country) => getCountry(country));
+
+  const container = document.getElementById("countries");
+  container.innerHTML = countriesHTML.join(" ");
+};
+
+const getCountry = (country) => {
+  console.log(country);
+  return `<div class="country-div">
+  <img src="${country.flags.png}">
+  <h2>${country.name.common}</h2>
+  <hr>
+  <h4>Population: ${country.population}</h4>
+  <h4>Region: ${country.region}</h4>
+  <h4>Capital: ${country.capital}</h4>
+  </div>`;
+};
+
+localCountryApi();
